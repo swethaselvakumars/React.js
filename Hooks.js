cd40924f87d683118eb0a1323ddfe160e725520f
Swetha Selvakumar <127839034+swethaselvakumars@users.noreply.github.com>
2024-08-05 17:07:45 +0530

    Add files via upload

---
+++ Hooks.js
@@ -0,0 +1,15 @@
+export const Hookk=()=>{
+    return(
+        <>
+        <h1>first component</h1>
+        </>
+    )
+}
+export function Hookkz(){
+    return(
+        <>
+        <h1>second component</h1>
+        </>
+    )
+}
+
