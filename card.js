c586def5a5776cb2c2b9464d315d9b730285da29
Swetha Selvakumar <127839034+swethaselvakumars@users.noreply.github.com>
2024-08-05 17:06:34 +0530

    Add files via upload

---
+++ card.js
@@ -0,0 +1,12 @@
+import { Bodyy } from "../body/body";
+
+export function Cardd(){
+    return(
+        <>
+        <Bodyy lion="Github" tiger="connect" ></Bodyy>
+        <Bodyy lion="Instagram" tiger="connect" ></Bodyy>
+        <Bodyy lion="LinkedIn" tiger="connect" ></Bodyy>
+        </>
+
+    )
+}
